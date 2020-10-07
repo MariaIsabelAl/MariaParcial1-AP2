6a160aebf0cec93d09f17854791dfb73f4c68e97
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-10-07 18:38:55 -0400

    Creacion de la Migracion

---
+++ Migrations/20201007223723_ProductoDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MariaParcial1_AP2.Migrations
+{
+    public partial class ProductoDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    productoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    descripcion = table.Column<string>(nullable: false),
+                    existencia = table.Column<int>(nullable: false),
+                    costo = table.Column<decimal>(nullable: false),
+                    valorInventario = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.productoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
