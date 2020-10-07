98de172a0edd78fece441755829a1980a5fd3c4e
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-10-07 18:20:19 -0400

    Entidad Producto

---
+++ Models/Producto.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2.Models
+{
+    public class Producto
+    {
+        [Key]
+        [Required(ErrorMessage ="Este campo no puede estar vacio.")]
+        [Range(0,100000, ErrorMessage ="El rango es de 0 a 100000")]
+        public int productoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        [Range(1,1000000, ErrorMessage ="El rango es de 1 a 100000")]
+        public int existencia { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        [Range(1, 1000000, ErrorMessage = "El rango es de 1 a 100000")]
+        public decimal costo { get; set; }
+
+        [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+        public decimal valorInventario { get; set; }
+
+        public Producto()
+        {
+            productoId = 0;
+            descripcion = string.Empty;
+            existencia = 0;
+            costo = 0;
+            valorInventario = 0;
+        }
+    }
+}
